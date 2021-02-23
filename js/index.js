42b4754cb1fbf0a474c1db3e4215d938029f0437
Ilaniya <evgeniya.pavlovich@gmail.com>
2021-02-23 21:56:32 +0200

    add anchor

--- js/index.js
+++ js/index.js
@@ -7,3 +7,16 @@
 //     document.getElementById("headerMenu").classList.toggle("active");
 //   }
 // };
+
+$(function () {
+  // Scroll Event
+  $(window).on("scroll", function () {
+    let scrolled = $(window).scrollTop();
+    if (scrolled > 1000) $(".arrow-bottom").addClass("active-arrow-up");
+    if (scrolled < 1000) $(".arrow-bottom").removeClass("active-arrow-up");
+  });
+  // Click Event
+  $(".arrow-bottom").on("click", function () {
+    $("html, body").animate({ scrollTop: "0" }, 300, "swing");
+  });
+});
